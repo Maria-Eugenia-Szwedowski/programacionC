9e44c7c9253d86c597868efd002c58407a684363
Maria-Eugenia-Szwedowski <76910855+Maria-Eugenia-Szwedowski@users.noreply.github.com>
2025-02-26 08:08:45 -0300

    Create p2e5.c

---
+++ p2e5.c
@@ -0,0 +1,18 @@
+// Escriba un programa que declare e inicialice dos variables y luego realice la suma y resta sobre ellas.
+// Desplegar los datos en pantalla como las operaciones en la escuela primaria
+
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+	
+	int a, b, resultSuma, resultResta;
+	a = 45;
+	b = 13;
+	resultSuma = a + b;
+	resultResta = a - b;
+	
+	printf("El valor entero sumado: %d\n ",resultSuma);
+	printf("El valor entero diferencia: %d\n ",resultResta);
+	
+}
